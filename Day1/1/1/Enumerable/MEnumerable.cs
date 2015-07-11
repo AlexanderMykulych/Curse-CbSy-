fb1fa1f378e19d020148ea7bdb6667c4f8eb49db
Alexander <alexander.mykulych@gmail.com>
2015-07-11 22:03:44 +0300

    1 day 1 task

---
+++ Day1/1/1/Enumerable/MEnumerable.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Enumerable {
+    public class MEnumerable : IEnumerable<int>, IEnumerator<int> {
+
+        private int position { get; set; }
+        private int lastIndex { get; set; }
+        private int[] elements { get; set; }
+        public MEnumerable() {
+            position = -1;
+            lastIndex = -1;
+            elements = new int[1];
+        }
+        public IEnumerator<int> GetEnumerator() {
+            position = -1;
+            while (true) {
+                if (position < elements.Count() - 1) {
+                    yield return elements[++position] * elements[position];
+                } else {
+                    yield break;
+                }
+            }
+        }
+        public int Current {
+            get { return elements[position]; }
+        }
+
+        public void Dispose() {
+            position = -1;
+        }
+
+        object System.Collections.IEnumerator.Current {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool MoveNext() {
+            position++;
+            return true;
+        }
+
+        public void Reset() {
+            throw new NotImplementedException();
+        }
+        public void Add(int element) {
+            if (lastIndex < elements.Length-1) {
+                elements[++lastIndex] = element;
+                return;
+            }
+            var newArray = new int[elements.Length * 2];
+            elements.CopyTo(newArray, 0);
+            newArray[++lastIndex] = element;
+            elements = newArray;
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Day1/1/1/Program.cs
@@ -0,0 +1,18 @@
+﻿using _1.Enumerable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1 {
+    class Program {
+        static void Main(string[] args) {
+            var collection = new MEnumerable() { 1, 2, 3, 4 };
+            foreach (var element in collection) {
+                Console.WriteLine(element);
+            }
+            Console.ReadKey();
+        }
+    }
+}
